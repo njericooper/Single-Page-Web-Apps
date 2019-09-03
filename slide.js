f75f2cb046c56956862bf434e9e21100f652eb91
Njeri <njericooper@gmail.com>
2019-09-03 09:03:07 -0500

    update

---
+++ slide.js
@@ -0,0 +1,18 @@
+//provide slider capability
+
+var spa = (function ( $ ){
+    //Module scope variable
+    var
+    //Set constraints
+    configMap = { },
+    //declare all other module scope variables
+    $chatSlider,
+    toggleSlider, onClickSlider, initModule;
+    // DOM method /toggleSlider/
+    // alternates slider height
+
+    toggleSlider = function () {};
+
+    // Event handler /onClickSlider/
+    // recieves click event and calls toggle
+})
